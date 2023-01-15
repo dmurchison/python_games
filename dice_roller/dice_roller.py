c09f1342e1c4e7d11d4f95b45bd2293288aa28c7
Duncan Murchison <duncancmurchison@gmail.com>
2023-01-15 11:23:13 -0500

    fixed spacing

--- dice_roller/dice_roller.py
+++ dice_roller/dice_roller.py
@@ -9,26 +9,27 @@ title.grid(row=0, column=1, columnspan=2)
 
 
 def roll_dice():
-  # Roll the dice
-  dice = [
-    '\u2680',
-    '\u2681',
-    '\u2682',
-    '\u2683',
-    '\u2684',
-    '\u2685',
-    ]
-  roll1 = random.choice(dice)
-  roll2 = random.choice(dice)
-  roll3 = random.choice(dice)
-  roll4 = random.choice(dice)
-  return roll1 + roll2 + roll3 + roll4
+    # Roll the dice
+    dice = [
+      '\u2680',
+      '\u2681',
+      '\u2682',
+      '\u2683',
+      '\u2684',
+      '\u2685',
+      ]
+    roll1 = random.choice(dice)
+    roll2 = random.choice(dice)
+    roll3 = random.choice(dice)
+    roll4 = random.choice(dice)
+    return roll1 + roll2 + roll3 + roll4
+
 
 def roll():
-  # Display the dice roll
-  dice_roll = roll_dice()
-  dice_label = Label(App, text=dice_roll, font=('Helvetica', 260))
-  dice_label.grid(row=4, column=0, columnspan=4)
+    # Display the dice roll
+    dice_roll = roll_dice()
+    dice_label = Label(App, text=dice_roll, font=('Helvetica', 260))
+    dice_label.grid(row=4, column=0, columnspan=4)
 
 
 roll_button = Button(App, text='Roll', command=roll)
